0eb5ad2c60ec2628ede972d70056fcbc7eea7c5d
Zuzana Krejcova <mykrme@gmail.com>
2012-03-09 20:30:17 +0100

    adding userManager mock implementation

---
+++ src/main/java/cz/muni/fi/pv/projekt/UserManagerImpl.java
@@ -0,0 +1,40 @@
+package cz.muni.fi.pv.projekt;
+
+import java.util.List;
+
+/**
+ *
+ * @author Zuzana Krejcova
+ */
+public class UserManagerImpl implements UserManager {
+
+    @Override
+    public void createUser(User usr) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void deleteUser(User usr) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void updateUser(User usr) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public User selectUserById(Long id) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public User selectUserByNick(String nick) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public List<User> selectAllUsers() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
