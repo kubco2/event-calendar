53d30f2cd6bb19956141c0f0c6ea1371cc2b163d
Jakub Janco <kubco2@gmail.com>
2012-03-23 15:01:02 +0100

    corrected JUnit tests for UserManagerImpl

--- src/test/java/cz/muni/fi/pv/projekt/UserManagerImplTest.java
+++ src/test/java/cz/muni/fi/pv/projekt/UserManagerImplTest.java
@@ -33,52 +33,78 @@ public class UserManagerImplTest {
 
     @Test
     public void createCorrectUser() throws Exception {
-        User usr1 = userTemplate(null, "Jakub", "kubco", "mypass");
+        User usr1 = userTemplate("Jakub", "kubco", "mypass");
         userManager.createUser(usr1);
-        User usr2 = userManager.selectUserByNick(usr1.getNick());
+        assertNotNull("createUser doesn't assign ID",usr1.getId());
+
+        User usr2 = userManager.selectUserById(usr1.getId());
         assertTrue("User was not created",usr2 != null);
-        assertTrue("Created user got null id",usr2.getId() == null);
         assertEquals("Created user got bad name",usr1.getName(),usr2.getName());
         assertEquals("Created user got bad nick",usr1.getNick(),usr2.getNick());
         assertEquals("Created user got bad password",usr1.getPassword(),usr2.getPassword());
     }
 
     @Test
+    public void updateNullUser() throws Exception {
+        User usr = null;
+        try {
+            userManager.updateUser(usr);
+            fail("Null user was updated");
+        } catch (NullPointerException ex) {
+            // OK
+        }
+    }
+
+    @Test
     public void updateUser() throws Exception {
-        Long id = (long)1;
-        User usr1 = userTemplate(id, "Jakub", "kubco", "mypass");
+        User usr1 = userTemplate("Jakub", "kubco", "mypass");
         userManager.createUser(usr1);
-        User usr2 = userTemplate(id, "Jakub1", "kubco1", "mypass1");
-        userManager.updateUser(usr2);
-        User usr3 = userManager.selectUserById(id);
-        Object[] usr2Props = {usr2.getName(),usr2.getNick(),usr2.getPassword()};
-        Object[] usr3Props = {usr3.getName(),usr3.getNick(),usr3.getPassword()};
-        assertArrayEquals("Created user was not updated",usr2Props,usr3Props);
+        usr1.setName("Jakub1");
+        usr1.setNick("kubco1");
+        usr1.setNick("mypass1");
+        userManager.updateUser(usr1);
+        User usr2 = userManager.selectUserById(usr1.getId());
+        assertEquals("Updated user got bad name",usr1.getName(),usr2.getName());
+        assertEquals("Updated user got bad nick",usr1.getNick(),usr2.getNick());
+        assertEquals("Updated user got bad password",usr1.getPassword(),usr2.getPassword());
+    }
+
+    @Test
+    public void deleteNullUser() throws Exception {
+        User usr = null;
+        try {
+            userManager.deleteUser(usr);
+            fail("Null user was deleted");
+        } catch (NullPointerException ex) {
+            // OK
+        }
+    }
+
+    @Test
+    public void deleteUser() throws Exception {
+        User usr = userTemplate("User name 1","nick1","pass1");
+        userManager.createUser(usr);
+        User usr1 = userManager.selectUserById(usr.getId());
+        assertNotNull(usr1);
+        userManager.deleteUser(usr);
+        usr1 = userManager.selectUserById(usr.getId());
+        assertNull("Manager didn't delete user",usr1);
     }
 
     @Test
     public void selectUserById() throws Exception {
-        Long id1 = (long)1;
-        Long id2 = (long)2;
-        Long id3 = (long)3;
-        Long id4 = (long)4;
-        User usr1 = userTemplate(id1,"User name 1","nick1","pass1");
-        User usr2 = userTemplate(id2,"User name 2","nick2","pass2");
-        User usr3 = userTemplate(id3,"User name 3","nick3","pass3");
-        User usr4 = userTemplate(id4,"User name 4","nick4","pass4");
+        User usr1 = userTemplate("User name 1","nick1","pass1");
+        User usr2 = userTemplate("User name 2","nick2","pass2");
         userManager.createUser(usr1);
         userManager.createUser(usr2);
-        userManager.createUser(usr3);
-        userManager.createUser(usr4);
-        User[] users = {usr3,usr1,usr4,usr2};
-        User[] usersGot = {userManager.selectUserById(id3),userManager.selectUserById(id1),
-                userManager.selectUserById(id4),userManager.selectUserById(id2)};
-        assertArrayEquals("SelectById method does not work correct",users,usersGot);
+        User[] users = {usr1,usr2};
+        User[] usersGot = {userManager.selectUserById(usr1.getId()),
+                userManager.selectUserById(usr2.getId())};
+        assertArrayEquals("SelectUserById method does not work correct",users,usersGot);
     }
 
-    private User userTemplate(Long id, String name, String nick, String pass) {
+    private User userTemplate(String name, String nick, String pass) {
         User user = new User();
-        user.setId(id);
         user.setName(name);
         user.setNick(nick);
         user.setPassword(pass);
